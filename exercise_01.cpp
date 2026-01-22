1deaed620f14b7a9becfef0a13d7125a431483cf
Aleksandr Kuzmich <blademaster852@hotmail.com>
2026-01-22 18:43:45 +0200

    small changes

--- exercise_01.cpp
+++ exercise_01.cpp
@@ -1,37 +1,66 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-// void my_function(int x , int y );
-//
-// int main(void)
-// {
-//     my_function(5, 5);
-//     return 0;
-// }
-//
-// /* Just a simple comment*/
-// void my_function(int x, int y)
-// {
-//     printf("Y = %d\n", y);
-//     printf("Old X = %d\n", x);
-//     x = x * y;
-//     printf("new X = %d\n", x);
-// }
 
-void my_function(int my_veriable, int b, char c);
+int main(void) {
 
-int main (void)
-{
-    my_function(0,0,'c');
-    return 0;
-}
+    float bus_price  = 0;
+    float taxi_price = 0;
+    float money = 0;
+    int selection;
+    bool enough_money = true;
 
-void my_function(int my_veriable, int b, char c )
-{
-    my_veriable = 5;
-    b = my_veriable + 5;
-    c = 'A';
-    printf ("my_veriable = %d\n", my_veriable);
-    printf ("b = %d\n", b);
-    printf ("c = %c\n", c);
+    printf("Enter price of bus ticket: ");
+    scanf("%f", &bus_price);
+    printf("Enter price of taxi ticket: ");
+    scanf("%f", &taxi_price);
+    printf("How much money you have: ");
+    scanf("%f", &money);
+
+    printf("You have %.2f euros left. \n", money );
+
+    while (enough_money) {
+
+        printf("Do you want to take \n");
+        printf("1) Bus (%.2f)\n", bus_price);
+        printf("2) Taxi (%.2f)\n", taxi_price);
+
+        printf("Enter selection: ");
+        scanf("%d", &selection);
+
+        if (money < bus_price && money < taxi_price) {
+            printf("You need to walk. Bye.");
+            enough_money = false;
+            break;
+        }
+
+
+        switch (selection) {
+
+            case 1:
+                printf("You chose bus. \n");
+                if (money < bus_price && money < taxi_price) {
+            printf("You need to walk. Bye.");
+            enough_money = false;
+            break;
+        }
+                if (money >= bus_price) {
+                    money -= bus_price;
+                    printf("You have %.2f euros left. \n", money );
+                } else {
+                    printf("You have %.2f euros left. \n", money );
+                    printf("You don't have enough money for bus.\n");
+                }
+                break;
+            case 2:
+                printf("You chose taxi. \n");
+                if (money >= taxi_price) {
+                    money -= taxi_price;
+                    printf("You have %.2f euros left.\n", money );
+                } else {
+                    printf("You have %.2f euros left. \n", money );
+                    printf("You don't have enough money for taxi.\n");
+                }
+                break;
+        }
+    }
 }
