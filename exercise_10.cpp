e1a0160163d49166fd7c35592dd186f7b5334299
Aleksandr Kuzmich <blademaster852@hotmail.com>
2026-02-11 10:38:59 +0200

    exercise_10 is starting..

--- exercise_10.cpp
+++ exercise_10.cpp
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
-int main () {
 
-    printf("Hello world!");
-
+int main(void)
+{
+    char string [256];
+    printf ("Insert your full address: ");
+    gets (string);
+    printf ("Your address is: %s\n",string);
+    printf("Length: %d\n ", strlen(string));
     return 0;
 }
-
 
 
 
