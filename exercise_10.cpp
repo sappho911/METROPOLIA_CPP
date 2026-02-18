b7409217cb8cc041ca360b63dbda3eb08f5cfa6a
Aleksandr Kuzmich <blademaster852@hotmail.com>
2026-02-18 15:22:24 +0200

    exercise_10 is done..

--- exercise_10.cpp
+++ exercise_10.cpp
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
+#define STR_SIZE 20
 
-int main(void)
-{
-    char string [256];
-    printf ("Insert your full address: ");
-    gets (string);
-    printf ("Your address is: %s\n",string);
-    printf("Length: %d\n ", strlen(string));
+int main() {
+    char str[STR_SIZE];
+
+    while (1) {
+        printf("Enter a string: ");
+        fgets(str, STR_SIZE, stdin);
+
+        if (str[strlen(str) - 1] == '\n') {
+            str[strlen(str) - 1] = '\0';
+        }
+
+        if (strcmp (str, "stop") == 0 ) {
+            printf("Stopped \n");
+            break;
+        }
+
+        printf("The string length is: %d \n", strlen(str));
+    }
+
     return 0;
 }
-
 
 
