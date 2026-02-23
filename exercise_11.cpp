8c594cdd6502f480d39d758f04dc92733ae6183d
Aleksandr Kuzmich <blademaster852@hotmail.com>
2026-02-23 15:43:00 +0200

    exercise_11 is done...

--- exercise_11.cpp
+++ exercise_11.cpp
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int replace_char(char *str, const char *repl);
 
+#define STR_SIZE 80
+#define STR_REPL_SIZE 3
+#define REPL_CHAR_NUM 2
+
 int main() {
 
-    char text[80] = "I am so tired of Python. C is much better language";
-    int count = replace_char(text, "e3");
+    char text[STR_SIZE];
+    char repl[STR_REPL_SIZE];
+
+    printf("Enter a text: ");
+    fgets(text, STR_SIZE, stdin);
+    printf("%s", text);
+    printf("Enter a character you want to replace (e3) (e gonna be replaced to 3):");
+    fgets(repl, STR_REPL_SIZE, stdin);
+
+    if (repl[strlen(repl) - 1] == '\n') {
+        repl[strlen(repl) - 1] = '\0';
+    }
+
+    int count = replace_char(text, repl);
 
     return 0;
 }
@@ -13,6 +30,12 @@ int main() {
 int replace_char(char *str, const char *repl) {
     int i;
     int count = 0;
+    int length = strlen(repl);
+
+    if (length != REPL_CHAR_NUM) {
+        printf("Enter two characters.\n");
+        return 0;
+    }
 
     for (i = 0; str[i] != '\0'; i++) {
 
