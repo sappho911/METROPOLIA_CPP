6290b11d8a4616ce6b66900379aab018128f5baf
Aleksandr Kuzmich <blademaster852@hotmail.com>
2026-02-23 16:32:46 +0200

    exercise_12 is done...

--- exercise_12.cpp
+++ exercise_12.cpp
@@ -1,14 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 
+#define STR_SIZE 80
+#define WORD_SIZE 80
+
+int count_words(const char* str, const char* word);
+
 int main() {
-    char s1[] = "Hello world!";
-    char s2[] = " world!";
+    char str[STR_SIZE];
+    char word[WORD_SIZE];
+    int res;
 
-    char *p;
+    while (1) {
+        printf("Enter a string:");
+        fgets(str, STR_SIZE, stdin);
 
-    p = strstr(s1, s2);
+        if (str[strlen(str) - 1] == '\n') {
+            str[strlen(str) - 1] = '\0';
+        }
 
-    printf("%s", p);
+        printf("Enter a word:");
+        fgets(word, WORD_SIZE, stdin);
 
+        if (word[strlen(word) - 1] == '\n') {
+            word[strlen(word) - 1] = '\0';
+        }
+
+        if (strcmp(word, "stop") == 0) {
+            break;
+        }
+        res = count_words(str, word);
+        printf("Counted matched words are: %d \n", res);
+    }
+    return 0;
+}
+
+int count_words(const char* str, const char* word) {
+
+    int count = 0;
+    const char* ptr = str;
+    int length = strlen(word);
+
+    while ((ptr = strstr(ptr, word)) != 0) {
+        count++;
+        ptr += length;
+    }
+    return count;
 }
